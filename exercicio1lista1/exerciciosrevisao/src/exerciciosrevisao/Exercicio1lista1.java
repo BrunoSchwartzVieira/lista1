62752ab9a561b72eedf5d822707821e9f5005bd8
Bruno Vieira <137101698+BrunoSchwartzVieira@users.noreply.github.com>
2024-08-30 15:19:35 -0300

    Add files via upload

---
+++ exercicio1lista1/exerciciosrevisao/src/exerciciosrevisao/Exercicio1lista1.java
@@ -0,0 +1,14 @@
+
+package exerciciosrevisao;
+
+
+public class Exercicio1lista1 {
+
+   
+    public static void main(String[] args) {
+       
+        System.out.println("Olá, mundo!");
+        
+    }
+    
+}
